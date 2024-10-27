6651b441a187e586f81c99f9e1f595a2bae75cbf
Andrei Dorian Duma <andrei@andreiduma.ro>
2024-10-27 14:24:26 +0200

    Test repetitions in Thompson's construction

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,13 @@ mod thompson;
 fn main() {
     println!("One day I will be a Cool compiler!");
 
-    let expr = regex::Expr::Alternation(vec![
+    let expr = regex::Expr::Repetition(Box::new(regex::Expr::Alternation(vec![
         regex::Expr::Concatenation(vec![
             regex::Expr::Character('a'),
             regex::Expr::Character('b'),
         ]),
         regex::Expr::Character('c'),
-    ]);
+    ])));
     dbg!(&expr);
 
     let nfa = thompsons_construction(expr);
